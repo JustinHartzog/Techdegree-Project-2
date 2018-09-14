04cd7e992d7b7bb971e03dc1f1470b23aae6fa7d
JustinHartzog <42506583+JustinHartzog@users.noreply.github.com>
2018-09-13 18:00:17 -0600

    fixed some issues
    fixed the fact that only 9 students showed up per page and dynamicaly added the div element with the class name pagination

--- js/script.js
+++ js/script.js
@@ -8,23 +8,29 @@ FSJS project 2 - List Filter and Pagination
 const ul = document.querySelector('.student-list');
 const lis = ul.children;
 const numPages = Math.ceil(lis.length / 10);
+const newDiv = document.createElement('div');
+const newUl = document.createElement('ul');
+newDiv.className = 'pagination';
+document.querySelector('.page').appendChild(newDiv);
+newDiv.appendChild(newUl);
 const div = document.querySelector('.pagination');
 for (let i = 1; i <= numPages; i += 1) {
   const li = document.createElement('li');
   var display = '';
   display = "<a href='#'>" + i + "</a>";
   li.innerHTML = display;
-  div.appendChild(li);
+  newUl.appendChild(li);
 }
 const pageOne = document.querySelector('div.pagination li a');
 pageOne.className = 'active';
+
 
 // This function hides all the students except for those that should be displayed
 function disappear(y, j) {
   for (let i = 0; i < y.length; i += 1) {
     let li = y[i];
     const top = j * 10;
-    const bottom = top - 10;
+    const bottom = top - 11;
     if(i < top && i > bottom) {
       li.style.display = 'block';
     } else {
